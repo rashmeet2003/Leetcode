de8dfb70a971120a11470a47545fc31f16dfe63e
Rashmeet kaur <rashmeet2903@gmail.com>
2025-12-04 21:57:09 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (22.77%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String longestCommonPrefix(String[] str) {
+        if(str == null || str.length == 0){
+            return "";
+        }
+        String prefix = str[0];
+        for(int i=0; i<str.length;i++){
+            while(!str[i].startsWith(prefix)){
+                prefix = prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty()) return "";
+            }
+        }
+        return prefix;
+    }
+}
