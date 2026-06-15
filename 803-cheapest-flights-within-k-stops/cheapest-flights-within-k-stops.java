b8ffa4009f316974e171104328c7b48c7a5ef6e8
Rashmeet kaur <rashmeet2903@gmail.com>
2026-06-16 00:10:30 +0530

    Time: 9 ms (22.76%) | Memory: 46.5 MB (54.80%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.java
@@ -16,41 +16,41 @@ public class Solution {
         }
     }
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0; i<flights.length; i++){
-            int from = flights[i][0];
-            int to = flights[i][1];
-            int cost = flights[i][2];
+       ArrayList<ArrayList<Pair>> adj = new ArrayList<>();
+       for(int i=0; i<n; i++){
+        adj.add(new ArrayList<>());
+       }
+       for(int i=0; i<flights.length; i++){
+        int from = flights[i][0];
+        int to = flights[i][1];
+        int cost = flights[i][2];
 
-            adj.get(from).add(new Pair(to,cost));
-        }
-        int dist[] = new int[n];
-        Arrays.fill(dist,Integer.MAX_VALUE);
-        
-        Queue<Tuple> q = new LinkedList<>();
-        q.add(new Tuple(0,src,0));
-        dist[src] = 0;
+        adj.get(from).add(new Pair(to,cost));
+       }
+       int dist[] = new int[n];
+       Arrays.fill(dist,Integer.MAX_VALUE);
+       dist[src] = 0;
 
-        while(!q.isEmpty()){
-            Tuple curr = q.poll();
-            int currStop = curr.stop;
-            int currNode = curr.node;
-            int currDist = curr.dist;
+       Queue<Tuple> q = new LinkedList<>();
+       q.add(new Tuple(0,src,0));
 
-            for(int i=0; i<adj.get(currNode).size(); i++){
-                int neigh = adj.get(currNode).get(i).to;
-                int distN = adj.get(currNode).get(i).cost;
+       while(!q.isEmpty()){
+        Tuple curr = q.poll();
+        int currStop = curr.stop;
+        int currDist = curr.dist;
+        int currNode = curr.node;
 
-                if(dist[neigh] > currDist + distN && k>= currStop){
-                    dist[neigh] = currDist + distN;
-                    q.add(new Tuple(currStop+1,neigh,dist[neigh]));     
-                }
+        for(int i=0; i<adj.get(currNode).size(); i++){
+            int neigh = adj.get(currNode).get(i).to;
+            int distN = adj.get(currNode).get(i).cost;
+            
+            if(dist[neigh] > currDist + distN && k>=currStop){
+                dist[neigh] = currDist + distN;
+                q.add(new Tuple(currStop+1,neigh,dist[neigh]));
             }
         }
-        if(dist[dst] == Integer.MAX_VALUE) return -1;
-        return dist[dst];
+       }
+       if(dist[dst] == Integer.MAX_VALUE) return -1;
+       return dist[dst];
     }
 }
